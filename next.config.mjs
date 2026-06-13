19314f50b2a649c2925de96b67f939e48e282413
Ritik Prajapat <ritik.prajapat@thinktaurus.com>
2026-06-13 15:06:44 +0530

    Bug Fix

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data:",
               "font-src 'self'",
